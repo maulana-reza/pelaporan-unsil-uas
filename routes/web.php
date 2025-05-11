05f9ad75ca96b15907358d23555ef843b382c0d7
maulana-reza <7392maulana@gmail.com>
2025-05-11 12:45:07 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,14 @@ Route::middleware([
 
     Route::get('/dashboard', function () {
         if (auth()->user()->hasRole('Superadmin')) {
-            return \App\Livewire\Admin\Dasbor::class;
+            return redirect()->route('admin-dasbor');
         } elseif (auth()->user()->hasRole('Admin')) {
-            return redirect()->route('');
+            return redirect()->route('admin-dasbor');
         } else {
             return redirect()->route('buat-laporan');
         }
     })->name('dashboard');
+    Route::get('/admin/dasbor', \App\Livewire\Admin\Dasbor::class)->name('admin-dasbor');
     Route::get('/laporan-masuk', \App\Livewire\Admin\LaporanMasuk::class)->name('laporan-masuk');
     Route::get('/klasifikasi', \App\Livewire\Admin\Klasifikasi::class)->name('klasifikasi');
     Route::get('/bidang-terkait', \App\Livewire\Admin\BidangTerkait::class)->name('bidang-terkait');
